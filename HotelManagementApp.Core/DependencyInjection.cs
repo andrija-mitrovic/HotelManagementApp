01aba6da85b9dab38b5867999bb71afa71c3cacd
andrija-mitrovic <57568556+andrija-mitrovic@users.noreply.github.com>
2021-01-14 17:57:06 +0100

    Initial Creation

---
+++ HotelManagementApp.Core/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelManagementApp.Core
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddCore(this IServiceCollection services)
+        {
+            services.ConfigureInjection();
+
+            return services;
+        }
+
+        private static void ConfigureInjection(this IServiceCollection services)
+        {
+
+        }
+    }
+}
---
+++ HotelManagementApp.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelManagementApp.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration config)
+        {
+            services.ConfigureDatabase(config);
+            services.ConfigureInjection();
+
+            return services;
+        }
+
+        private static void ConfigureDatabase(this IServiceCollection services, IConfiguration config)
+        {
+
+        }
+
+        private static void ConfigureInjection(this IServiceCollection services)
+        {
+
+        }
+    }
+}
