cac862335a874731ac698a05b88bb5d70656a432
andrija-mitrovic <57568556+andrija-mitrovic@users.noreply.github.com>
2021-01-31 09:24:07 +0100

    RoomService added

---
+++ HotelManagementApp.Core/Services/RoomService.cs
@@ -0,0 +1,23 @@
+﻿using HotelManagementApp.Core.Interfaces.Data;
+using HotelManagementApp.Core.Interfaces.Services;
+using HotelManagementApp.Core.Models;
+using System;
+using System.Collections.Generic;
+
+namespace HotelManagementApp.Core.Services
+{
+    public class RoomService : IRoomService
+    {
+        private readonly IRoomRepository _roomRepository;
+
+        public RoomService(IRoomRepository roomRepository)
+        {
+            _roomRepository = roomRepository;
+        }
+
+        public List<Room> GetAvailableRooms(DateTime startDate, DateTime endDate, int roomTypeId)
+        {
+            return _roomRepository.GetAvailableRooms(startDate, endDate, roomTypeId);
+        }
+    }
+}
